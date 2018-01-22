31663df742d0411de175bc2515e2dbe22f08e572
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-01-22 17:02:57 +0300

    Added Macros Working

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_movies
 
 # Views
 
@@ -9,9 +10,18 @@ def index():
     """
     View root page function that returns the index page and its data
     """
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    upcoming_movies = get_movies('upcoming')
+    now_showing_movies = get_movies('now_playing')
+
     # message = 'Mans not hot'
     title = "Home - Welcome to The Best Movie Review Website Online"
-    return render_template('index.html', title=title)
+    return render_template('index.html',
+                           title=title,
+                           popular=popular_movies,
+                           upcoming=upcoming_movies,
+                           now_showing=now_showing_movies)
 
 
 @app.route('/movie/<int:movie_id>')
